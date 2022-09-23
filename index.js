2712aaedd827141ba7f4e5a6185e32cfa9e3c652
Connor Vosberg <connorjv@gmail.com>
2022-09-23 13:58:38 -0700

    commit tree 773c800791ba6524acc5d92fdb7c185a1944c47f

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a,b) 
+{
+return a + b
+}
+
+function subtract(a,b)
+{
+return a - b
+}
+
+function multiply(a,b)
+{
+return a * b
+}
+
+function divide(a,b)
+{
+return a / b
+}
+
+function increment(n)
+{
+    return n + 1
+}
+
+function decrement(n)
+{
+    return n - 1
+}
+
+function makeInt(string)
+{
+    parseInt(string, 10)
+    return parseInt(string, 10)
+}
+
+function preserveDecimal(string)
+{
+    parseFloat(string)
+    return parseFloat(string)
+}
